e22f75403eb63572e40c56ca91c6e07324e18d0d
kaiostavares <kaiosandek@outlook.com>
2023-10-12 14:58:49 -0300

    construção backEnd

--- src/main/java/br/com/kaiotavares/todolist/TodolistApplication.java
+++ src/main/java/br/com/kaiotavares/todolist/TodolistApplication.java
@@ -3,7 +3,9 @@ package br.com.kaiotavares.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //Anotation
+//Nesse caso essa anotation é uma interface, essa anotatio do spring vai definir que essa é a classe inicial do projeto
+//Quando iniciamos o projeto temos um tomcat por de baixo dos panos rodando o projeto
 public class TodolistApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/kaiotavares/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.kaiotavares.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+}
