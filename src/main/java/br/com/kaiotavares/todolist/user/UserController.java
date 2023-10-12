4cde1753b2651be2b24bd1eff07021985622d6b9
kaiostavares <kaiosandek@outlook.com>
2023-10-12 16:44:36 -0300

    create repository

--- src/main/java/br/com/kaiotavares/todolist/user/UserController.java
+++ src/main/java/br/com/kaiotavares/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.kaiotavares.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired //Todo ciclo é gerenciado pelo Spring
+    private IUserRepository userRepository;
+
     @PostMapping("/") //Como a gente está criando, vêm de um método POST
-    public void create(@RequestBody UserModel userModel){ //ResquestBody significa que oq eu estou recebendo o body da requisição
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel){ //ResquestBody significa que oq eu estou recebendo o body da requisição
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
