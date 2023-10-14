c5d6b1a4c56db40bf601dddafdfe76bb9387aa2b
kaiostavares <kaiosandek@outlook.com>
2023-10-14 14:27:25 -0300

    ownUserValidation

--- src/main/java/br/com/kaiotavares/todolist/task/TaskController.java
+++ src/main/java/br/com/kaiotavares/todolist/task/TaskController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -54,9 +55,21 @@ public class TaskController {
     // htpp://localhost:8080/tasks/9128918-asjgahsha-21776217
     @PutMapping("/{id}")
     //PathVariable relacionado ao id correspondente a determinada task da nossa aplicação
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
         var task = this.taskRepository.findById(id).orElse(null);
+
+        if(task==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+        if(!task.getIdUSer().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Usuário não têm permissão para alterar essa tarefa");
+        }
         Utils.copyNonNullProperties(taskModel, task);//Mesclando os dados nulos com não nulos
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
